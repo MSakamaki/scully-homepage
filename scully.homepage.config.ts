45246f4f966ae8ba1b32d62c6369f52ac557b175
M.Sakamaki <mail@m-sakamaki.jp>
2020-10-12 18:12:18 +0900

    feat: append scully (#4)
    * feat: append scully
    
    * feat: ncu update command

---
+++ scully.homepage.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/homepage/src",
+  projectName: "homepage",
+  outDir: './dist/static',
+  routes: {
+  }
+};
