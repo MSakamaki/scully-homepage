e2d52e58a59c6bfa3453f8d2afaeb7ecc5fd303c
MSakamaki <mail@m-sakamaki.jp>
2020-10-14 12:11:09 +0900

    fix(api): deploy setting

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,20 +2,41 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 
-import { Logger } from '@nestjs/common';
+import { Express } from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
+const server: Express = express();
 
-bootstrap();
+/**
+ * @command
+ * PORT=3333 npm start api
+ *  => http://localhost:3333/api/hello
+ * npm run build api && npx firebase serve --only functions
+ *  => http://localhost:5000/scully-homepage-5df8c/us-central1/api
+ */
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance)
+  );
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  if (process.env.PORT) {
+    app.listen(process.env.PORT);
+  }
+  console.log('the server is starting @ firebase');
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
