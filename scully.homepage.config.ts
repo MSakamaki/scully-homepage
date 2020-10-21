4d62937e7cca79d56c7164891c6003a683190758
M.Sakamaki <mail@m-sakamaki.jp>
2020-10-21 17:27:11 +0900

    feat: apollo graphql + scully (#11)

--- scully.homepage.config.ts
+++ scully.homepage.config.ts
@@ -1,8 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
-  projectRoot: "./apps/homepage/src",
-  projectName: "homepage",
+  projectRoot: './apps/homepage/src',
+  projectName: 'homepage',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
 };
