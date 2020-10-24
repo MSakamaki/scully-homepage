1622eb73b3c5250ff3c440e273bf58af648b63d4
MSakamaki <mail@m-sakamaki.jp>
2020-10-24 20:37:16 +0900

    feat: add visualize test

--- apps/homepage-e2e/src/support/commands.ts
+++ apps/homepage-e2e/src/support/commands.ts
@@ -1,3 +1,7 @@
+import {
+  addMatchImageSnapshotCommand,
+  SnapshotOptions,
+} from 'cypress-image-snapshot/command';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -8,16 +12,41 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
-declare namespace Cypress {
-  interface Chainable<Subject> {
-    login(email: string, password: string): void;
-  }
-}
+declare namespace Cypress {}
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+/**
+ * https://github.com/jaredpalmer/cypress-image-snapshot#options
+ */
+addMatchImageSnapshotCommand({
+  failureThresholdType: 'percent',
+  customDiffConfig: { threshold: 0.1 },
+  customSnapshotsDir: 'src/snapshot',
+  dumpDiffToConsole: true,
 });
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Screenshot test the current page.
+       * @param name file name of the screenshot
+       * @param options general options
+       */
+      matchImageSnapshot(
+        name: string,
+        options?: SnapshotOptions
+      ): Chainable<Element>;
+
+      /**
+       * Screenshot test the current page.
+       * @param options general options
+       */
+      matchImageSnapshot(options?: SnapshotOptions): Chainable<Element>;
+    }
+  }
+}
+
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
