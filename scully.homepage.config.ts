fa6df4fcddca010213679b0f31c7bf6752017f97
M.Sakamaki <mail@m-sakamaki.jp>
2020-10-28 08:05:58 +0900

    feat: add scully sitemap plugin ( and robots.txt ) (#32)
    * feat: add scully sitemap plugin
    
    * fix: add robots.txt

--- scully.homepage.config.ts
+++ scully.homepage.config.ts
@@ -1,4 +1,30 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
+
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://scully-homepage-5df8c.web.app/',
+  sitemapFilename: 'sitemap.xml',
+  merge: false,
+  trailingSlash: true,
+  changeFreq: 'monthly',
+  priority: [
+    '1.0',
+    '0.9',
+    '0.8',
+    '0.7',
+    '0.6',
+    '0.5',
+    '0.4',
+    '0.3',
+    '0.2',
+    '0.1',
+    '0.0',
+  ],
+  ignoredRoutes: ['/404'],
+  routes: {},
+});
+
 export const config: ScullyConfig = {
   projectRoot: './apps/homepage/src',
   projectName: 'homepage',
