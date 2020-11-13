516391604a3381f424decadc6fd4f8cde9df653e
mihirogi <dev.mihirogi@gmail.com>
2020-11-13 19:30:19 +0900

    add markdown

--- apps/homepage/src/app/blog/blog.module.ts
+++ apps/homepage/src/app/blog/blog.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { MarkdownModule } from 'ngx-markdown';
 import { HomeComponent } from './components/route/home/home.component';
 import { ArticleComponent } from './components/route/article/article.component';
 
 @NgModule({
   declarations: [HomeComponent, ArticleComponent],
-  imports: [CommonModule, BlogRoutingModule],
+  imports: [CommonModule, BlogRoutingModule, MarkdownModule.forRoot()],
   exports: [HomeComponent, ArticleComponent],
 })
 export class BlogModule {}
--- apps/homepage/src/app/blog/components/route/article/article.component.spec.ts
+++ apps/homepage/src/app/blog/components/route/article/article.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ApolloTestingModule } from 'apollo-angular/testing';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { ArticleComponent } from './article.component';
 
@@ -10,7 +11,11 @@ describe('ArticleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApolloTestingModule, RouterTestingModule],
+      imports: [
+        ApolloTestingModule,
+        RouterTestingModule,
+        MarkdownModule.forRoot(),
+      ],
       declarations: [ArticleComponent],
     }).compileComponents();
   });
