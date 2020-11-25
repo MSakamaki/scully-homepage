43d20ba5477f76e82f61fd36db997436440e0be0
mihirogi <dev.mihirogi@gmail.com>
2020-11-26 01:05:13 +0900

    add collect url

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      url: [
+        'https://scully-homepage-5df8c--pr54-setup-lhci-server-48jb4exz.web.app',
+      ],
+    },
     upload: {
       target: 'lhci',
       serverBaseUrl: 'https://still-island-43535.herokuapp.com/',
