ab5e90a6fb4ffd21b6bc2002064cb76465a6a2b2
mihirogi <dev.mihirogi@gmail.com>
2020-11-30 20:59:11 +0900

    feat(ci): add lighthouse-server setting (#54)
    * add lighthouse-server setting
    
    * add github-action for lhci
    
    * fix github action
    
    * fix node version
    
    * fix node version
    
    * add collect url
    
    * fix action
    
    * fix lighthouse-setting
    
    * fix url
    
    * set localhost

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:1668'],
+    },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://still-island-43535.herokuapp.com/',
+      token: 'b0591ec8-83cc-41bc-a0a7-a5f4a77212eb',
     },
   },
 };
