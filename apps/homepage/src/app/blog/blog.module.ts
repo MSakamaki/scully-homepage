63823a29ba8f14c6908da94ee932f7281acab34c
mihirogi <dev.mihirogi@gmail.com>
2021-01-22 19:48:09 +0900

    feat:markdown img width to 100%

--- apps/homepage/src/app/blog/blog.module.ts
+++ apps/homepage/src/app/blog/blog.module.ts
@@ -6,13 +6,32 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { MarkdownModule } from 'ngx-markdown';
 import { HomeComponent } from './components/route/home/home.component';
 import { ArticleComponent } from './components/route/article/article.component';
+import { MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+
+// function that returns `MarkedOptions` with renderer override
+export const markedOptionsFactory = (): MarkedOptions => {
+  const renderer = new MarkedRenderer();
+
+  renderer.image = (href: string, title: string, text: string) => {
+    return `<img class="image" src="${href}" alt="${text}" title="${title}" width=100%></img>`;
+  };
+
+  return {
+    renderer: renderer,
+  };
+};
 
 @NgModule({
   declarations: [HomeComponent, ArticleComponent],
   imports: [
     CommonModule,
     BlogRoutingModule,
-    MarkdownModule.forRoot(),
+    MarkdownModule.forRoot({
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory,
+      },
+    }),
     SharedUiModule,
   ],
   exports: [HomeComponent, ArticleComponent],
